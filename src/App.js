452f3f0ec2abe5aaa0925b064dbe7ecb0988fdc7
Gustavo Lira <gustavolira8813@gmail.com>
2021-09-12 21:57:40 -0300

    finalizado

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import { useState } from "react";
+import { useEffect } from "react";
+import CharCard from "./components/CharCard";
+import Characters from "./components/Characters";
+function App() {
+  const [characterList, setCharacterList] = useState([]);
+  useEffect(() => {
+    fetch("https://rickandmortyapi.com/api/character/")
+      .then((response) => response.json())
+      .then((response) => setCharacterList(response.results))
+      .catch((err) => console.log(err));
+  }, []);
+  console.log(characterList);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Characters characterList={characterList} />
+      </header>
+    </div>
+  );
+}
+
+export default App;
