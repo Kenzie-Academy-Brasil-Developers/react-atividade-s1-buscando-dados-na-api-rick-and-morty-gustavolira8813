810c6dbbcf94be82068129c99560245976201049
gustavolira8813 <gustavolira8813@gmail.com>
2021-11-16 12:02:08 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>teste</h1>
         <Characters characterList={characterList} />
       </header>
     </div>
